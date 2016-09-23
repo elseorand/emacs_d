4055e4c3ddde9e66298aea70798175ac829e6d7c
elseorand <sumita@fg8.so-net.ne.jp>
2016-09-24 01:12:01 +0900

    add xwidget webkit keybindings

--- init.el
+++ init.el
@@ -1363,13 +1363,6 @@
 ;; slack end
 
 ;; webkit start
-;; make these keys behave like normal browser
-;; (define-key xwidget-webkit-mode-map [mouse-4] 'xwidget-webkit-scroll-down)
-;; (define-key xwidget-webkit-mode-map [mouse-5] 'xwidget-webkit-scroll-up)
-;; (define-key xwidget-webkit-mode-map (kbd "<up>") 'xwidget-webkit-scroll-down)
-;; (define-key xwidget-webkit-mode-map (kbd "<down>") 'xwidget-webkit-scroll-up)
-;; (define-key xwidget-webkit-mode-map (kbd "M-w") 'xwidget-webkit-copy-selection-as-kill)
-;; (define-key xwidget-webkit-mode-map (kbd "C-c") 'xwidget-webkit-copy-selection-as-kill)
 
 ;; adapt webkit according to window configuration chagne automatically
 ;; without this hook, every time you change your window configuration,
@@ -1378,6 +1371,20 @@
                (when (equal major-mode 'xwidget-webkit-mode)
 		 (linum-mode 0)
                  (xwidget-webkit-adjust-size-dispatch))))
+
+(add-hook 'xwidget-webkit-mode-hook
+	  (function (lambda ()
+		      (linum-mode 0)
+		      ;; make these keys behave like normal browser
+		      (define-key xwidget-webkit-mode-map [mouse-4] 'xwidget-webkit-scroll-down)
+		      (define-key xwidget-webkit-mode-map [mouse-5] 'xwidget-webkit-scroll-up)
+		      (define-key xwidget-webkit-mode-map (kbd "<up>") 'xwidget-webkit-scroll-down)
+		      (define-key xwidget-webkit-mode-map (kbd "<down>") 'xwidget-webkit-scroll-up)
+		      (define-key xwidget-webkit-mode-map (kbd "M-w") 'xwidget-webkit-copy-selection-as-kill)
+		      (define-key xwidget-webkit-mode-map (kbd "C-/") 'xwidget-webkit-back)
+		      (define-key xwidget-webkit-mode-map (kbd "C-l") 'xwidget-webkit-reload)
+		      (define-key xwidget-webkit-mode-map (kbd "C-c C-s") 'xwidget-webkit-bookmark-make-record)
+)))
 
 ;; by default, xwidget reuses previous xwidget window,
 ;; thus overriding your current website, unless a prefix argument
